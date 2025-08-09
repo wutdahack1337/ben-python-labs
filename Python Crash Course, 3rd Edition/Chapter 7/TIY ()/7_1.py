87ccc26f3d8d928cbddcaf61a870cb9d536a7303
wutdahack1337 <wutdahack1337@gmail.com>
2025-08-09 18:14:01 +0700

    hi

---
+++ Python Crash Course, 3rd Edition/Chapter 7/TIY ()/7_1.py
@@ -0,0 +1,6 @@
+mess = "Bạn đang tìm chiếc xe nào?"
+mess += "\nHãy nói với tôi: "
+
+qst = input(mess)
+
+print(f"Để tôi xem có thể tìm cho bạn một chiếc {qst} không.")
---
+++ Python Crash Course, 3rd Edition/Chapter 7/TIY ()/7_3.py
@@ -0,0 +1,3 @@
+mess = int(input("Nhập một số: "))
+
+print(f"{mess} là bội của 10" if mess%10 == 0 else f"{mess} không phải là bội của 10")
