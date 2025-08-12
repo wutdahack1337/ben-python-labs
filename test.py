d8c3b13ef2395bd044d2fbb692f0aba9c07c08f7
wutdahack1337 <wutdahack1337@gmail.com>
2025-08-12 10:44:13 +0700

    hi

--- test.py
+++ test.py
@@ -1,4 +1,19 @@
-def greeting(name, born_year):
-    print(f"Hello {name}.")
-    print(f"Your age is {2025-born_year}.")
-greeting("Khang", 2012)
+def family():
+    number_members = int(input("How many members are there in your family?: "))
+
+    i = 1
+
+    while i <= number_members:
+        member_name = input(f"mem{i}: Name: ")
+        member_by = input(f"mem{i}: Born_year: ")
+
+        print("="*45)
+
+        oldest_person = min(member_by)
+        youngest_person = max(member_by)
+        i += 1
+    print(oldest_person)
+        
+family()
+
+
