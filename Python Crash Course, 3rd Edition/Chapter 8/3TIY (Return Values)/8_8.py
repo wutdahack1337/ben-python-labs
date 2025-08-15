c778207d7b92b98e5434aec59f09c770f86782b3
wutdahack1337 <wutdahack1337@gmail.com>
2025-08-15 22:03:12 +0700

    hi

--- Python Crash Course, 3rd Edition/Chapter 8/3TIY (Return Values)/8_8.py
+++ Python Crash Course, 3rd Edition/Chapter 8/3TIY (Return Values)/8_8.py
@@ -2,18 +2,22 @@ def make_album():
     print()
     print("Bạn có thể nhập \"quit\" tại các câu hỏi để thoát.")
     print("----------------------------------------------------")
-    album = {}
+    album_info = {}
 
     while True:
         artist = input("Nghệ sĩ bạn ưa thích là ai?: ")
-        if artist == "quit":
-            break
-        else:
-            album['artist']
-        album = input("Album bạn ưa thích của người nghệ sĩ đó?: ")
-        if album == "quit":
+        if artist.lower() == "quit":
             break
 
+        album = input("Album bạn ưa thích của người nghệ sĩ đó?: ")
+        if album.lower() == "quit":
+            break
+
+        album_info[artist] = album
+        
+    for k, v in album_info.items():
+        print(f"{k}: {v}")
 
 
 make_album()
+
